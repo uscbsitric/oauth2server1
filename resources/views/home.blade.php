086e3e73ea2645458b43ca7de395c17025de868d
frederick sandalo <uscbsitric@gmail.com>
2017-06-16 12:54:45 +0000

    ambot nano ni pwerting daghanang changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,18 +5,29 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-                  Passport Clients:
-                  <passport-clients></passport-clients>
-                  Passport Authorized Clients:
-                  <passport-authorized-clients></passport-authorized-clients>
-                  Passport Personal Access Tokens:
-                  <passport-personal-access-tokens></passport-personal-access-tokens>
+                <div class="panel-heading">Dashboard</div>                  
                 <div class="panel-body">
                     You are logged in!
                 </div>
             </div>
         </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-8 col-md-offset-2">
+        <div class="panel panel-default">
+          <div class="panel-heading"> OAuth2 Operations </div>
+          
+          <div class="panel-body">
+            Passport Clients:
+            <passport-clients></passport-clients>
+            Passport Authorized Clients:
+            <passport-authorized-clients></passport-authorized-clients>
+            Passport Personal Access Tokens:
+            <passport-personal-access-tokens></passport-personal-access-tokens>
+          </div>
+        </div>
+      </div>
     </div>
 </div>
 @endsection
