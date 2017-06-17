f6a1198ce7418c1745544d65a58264c4d3673a9e
frederick sandalo <uscbsitric@gmail.com>
2017-06-17 17:59:57 +0000

    added the passport components to the home.blade.php template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,5 +13,19 @@
             </div>
         </div>
     </div>
+    
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">OAuth2 Operations</div>
+
+                <div class="panel-body">
+					<passport-clients></passport-clients>
+					<passport-authorized-clients></passport-authorized-clients>
+					<passport-personal-access-tokens></passport-personal-access-tokens>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
