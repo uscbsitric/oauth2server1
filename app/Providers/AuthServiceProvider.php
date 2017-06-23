888353c081384bb5eb7a67bcd0f1f49b92cdd6c7
frederick sandalo <uscbsitric@gmail.com>
2017-06-23 09:04:58 +0000

    Testing Token Lifetimes

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -27,5 +27,7 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Passport::routes();
+        Passport::tokensExpireIn(Carbon::now()->addMinute());
+        Passport::refreshTokensExpireIn(Carbon::now()->addMinute);
     }
 }
