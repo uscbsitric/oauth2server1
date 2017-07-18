e2ec84bcb4ee72342a083dd0444481814b1efaf1
frederick sandalo <uscbsitric@gmail.com>
2017-07-18 11:57:00 +0000

    Client Credentials Grant Setup and updated my git credentials again

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,6 +40,13 @@ class Kernel extends HttpKernel
             'throttle:60,1',
             'bindings',
         ],
+    	
+    	/*****
+        'client_credentials' => [\Laravel\Passport\Http\Middleware\CheckClientCredentials::class,
+        		                 'throttle:60,1',
+                                 'bindings',
+                                ]
+        *****/
     ];
 
     /**
@@ -58,5 +65,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
     	'scopes' => \Laravel\Passport\Http\Middleware\CheckScopes::class,
     	'scope' => \Laravel\Passport\Http\Middleware\CheckForAnyScope::class,
+    	'client_credentials' => \Laravel\Passport\Http\Middleware\CheckClientCredentials::class
     ];
 }
--- routes/api.php
+++ routes/api.php
@@ -21,20 +21,21 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::get('/user/{user}',
 		   function(App\User $user)
 		   {
-	         /*****
 	         $id = $user->id;
 	         $name = $user->name;
 	         $email = $user->email;
-	         
+ 
 	         return response()->json(['id' => $id,
 	         		                  'name' => $name,
 	         		                  'email' => $email
 	                                 ]);
-	         *****/
-		   	return response()->json(['id' => 'ID here',
-						   			 'name' => 'Name HERE',
-						   			 'email' => 'email here'
-						   			]);
+	         
+	         
+		   	 //return response()->json(['id' => 'ID here',
+			 //			   			  'name' => 'Name HERE',
+			 //			   			  'email' => 'email here'
+			 //			   			 ]);
+			 
            }
            )//->middleware('auth:api','scopes:check-status,place-orders'); // both these notations for middleware works
            ->middleware(['auth:api','scopes:check-status,place-orders']);  // both these notations for middleware works
