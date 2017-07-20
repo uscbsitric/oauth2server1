0c63e96b907580c39e859d1a5730fb46bcc918b4
Uchiha Madara <uchihamadara@Fredericks-MacBook-Pro.local>
2017-07-20 10:36:54 +0800

    fix for missing 'phone' field in the Users Table

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -342,6 +342,7 @@ class ComposerStaticInit498534e7ea1113e750603515bcd32c20
     );
 
     public static $classMap = array (
+        'AddUserPhoneColumn' => __DIR__ . '/../..' . '/database/migrations/2017_07_20_022649_add_user_phone_column.php',
         'App\\Console\\Kernel' => __DIR__ . '/../..' . '/app/Console/Kernel.php',
         'App\\Exceptions\\Handler' => __DIR__ . '/../..' . '/app/Exceptions/Handler.php',
         'App\\Http\\Controllers\\Auth\\ForgotPasswordController' => __DIR__ . '/../..' . '/app/Http/Controllers/Auth/ForgotPasswordController.php',
@@ -350,6 +351,7 @@ class ComposerStaticInit498534e7ea1113e750603515bcd32c20
         'App\\Http\\Controllers\\Auth\\ResetPasswordController' => __DIR__ . '/../..' . '/app/Http/Controllers/Auth/ResetPasswordController.php',
         'App\\Http\\Controllers\\Controller' => __DIR__ . '/../..' . '/app/Http/Controllers/Controller.php',
         'App\\Http\\Controllers\\HomeController' => __DIR__ . '/../..' . '/app/Http/Controllers/HomeController.php',
+        'App\\Http\\Controllers\\TestController' => __DIR__ . '/../..' . '/app/Http/Controllers/TestController.php',
         'App\\Http\\Kernel' => __DIR__ . '/../..' . '/app/Http/Kernel.php',
         'App\\Http\\Middleware\\EncryptCookies' => __DIR__ . '/../..' . '/app/Http/Middleware/EncryptCookies.php',
         'App\\Http\\Middleware\\RedirectIfAuthenticated' => __DIR__ . '/../..' . '/app/Http/Middleware/RedirectIfAuthenticated.php',
